2f07f77a7b45b1ee58c87640ad1cf69b61d1eb59
Luciana Masso <lucianamasso89@gmail.com>
2021-08-29 17:36:20 +0200

    tarea de typeahead

---
+++ Typeahaead/scrip.js
@@ -0,0 +1,56 @@
+let input = document.getElementById ("lista_input");
+
+let lista = document.getElementById ("lista");
+
+input.addEventListener ("keyup", (event)=> {
+
+    lista.style.display ="block";
+
+    let key = event.target.value;
+
+    if (key.length>0){
+        search (key);
+    }
+});
+
+const search = (key) => {
+
+    fetch(`https://restcountries.eu/rest/v2/name/${key}?fields=name`)
+        .then( res => res.json())
+        .then ( data => {
+            
+            if (Array.isArray(data)){
+
+                arma_lista(
+
+                    data.map((item)=>{
+                    return item.name;
+                })
+        );
+} 
+
+else {
+    arma_lista ()
+}
+
+    })
+    .catch(e => {console.log(e)})
+}
+
+const arma_lista = (items = []) => {
+
+    lista.innerHTML = "";
+
+    items.map( (item) => {
+        lista.innerHTML += `<li>${item}</li>`
+    })
+}
+
+
+lista.addEventListener("click",(e)=> {
+
+    if (e.target && e.target.nodeName == "LI"){
+        input.value = e.target.innerHTML;
+        arma_lista ();
+    }
+}); 
