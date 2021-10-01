3e0652ce0d43d76bef3f535751fcb527abeed498
Luciana Masso <lucianamasso89@gmail.com>
2021-10-01 15:02:33 +0200

    casi como siempre

--- Manejador de Tareas/script.js
+++ Manejador de Tareas/script.js
@@ -1,11 +1,18 @@
-const listaDeCard = [];
+
+const tareasFromStorage = localStorage.getItem ('listaDeCard');
+const tareasFronStorageObject = JSON.parse(tareasFromStorage);
+const taskList = tareasFronStorageObject || [];
+const listaDeCard = taskList.map( task => new Task_card (task));
 
 const formulario = document.querySelector ("#formulario")
 const name = document.querySelector ("#tarea_card")
 const description_form= document.querySelector ("#description_card")
 const priority_form = document.querySelector ("#priority_card")
 const info = document.querySelector ("#info_card")
-const tasks = document.querySelector("#tasks")
+const tasksp = document.querySelector("#taskspendiente")
+const tasksprogreso = document.querySelector("#tasksprogreso")
+const tasksrealizada = document.querySelector("#tasksrealizada")
+
 
 formulario.addEventListener ('submit', evt => {
     evt.preventDefault();
@@ -15,82 +22,120 @@ formulario.addEventListener ('submit', evt => {
     const description = form.description.value;
     const priority = form.priority.value;
     
-    const task = new Task_card (title, description,priority);
-
+    const task = new Task_card ({id: listaDeCard.length, title, description,priority});
     listaDeCard.push(task);
-    compleateCard();
-    cambioDeColor();
-    form.reset();  
-});
+    localStorage.setItem('listaDeCard',JSON.stringify(listaDeCard));
 
- compleateCard = ()=> {
-    let tasksView = document.querySelector('#tasks');
-    let addTask = document.createElement('div');
-        
-        listaDeCard.forEach (task => {     
-       
-            if (task.estado === 'pendiente') {
-            
-           addTask.innerHTML = "";
-        }
-        addTask.innerHTML = ` <div class=""card text-dark bg-light mb-3" " class="form-control">
-        <div class="card-header" class = "card-body"><h1>${task.title}</h1></div>
-        <div class = "card-body" ><p>${task.description}</p>
-        <a  onClick = "borrar(event)" class  ="btn btn-primary ml-2"> Delete Task </a>
-        <a  id = "btn" class  ="btn btn-primary ml-2"  data-id = ${task.id}> Estado : ${task.estado}</a>
-        </div></div>`
-        
-        tasksView.appendChild(addTask);
-           
-    })
+    compleateCard();
+    form.reset();  
+}); 
+
+compleateCard = ()=> {
     
+    let taskPendiente='';
+    let taskProgreso='';
+    let taskRealizada='';
+        
+    listaDeCard.forEach (task => {     
+       
+            if (task.estado === 'Pendiente') {
+            
+                taskPendiente += dibujarElemento (task) ;
+
+        } else if (task.estado === 'Progreso') {
+
+            taskProgreso += dibujarElemento (task)  ;
+        } else {
+            taskRealizada += dibujarElemento (task)  ;
+        }   
+    })       
+        taskspendiente.innerHTML = taskPendiente;
+        tasksprogreso.innerHTML = taskProgreso;
+        tasksrealizada.innerHTML = taskRealizada;
+}
+ 
+const dibujarElemento = (task) => {
+    
+        if(task.priority ===  'High'){
+
+            task = ` <li <div class = "card text-dark bg-light mb-2 " class="form-control"> 
+            <h5 class="card-header" class = "card-body" >${task.title}</h5> 
+            <p  class = "card-body"  > ${task.description}</p>
+            <a  id = "btn" type="button" class="btn btn-secondary "  data-id = ${task.id}> Estado : ${task.estado}</a>
+            <a  id = "btn2" type="button" class="btn btn-danger" > Estado : ${task.priority}</a>
+            </div></li>`
+        } else {
+
+            task = ` <li <div class="card text-dark bg-light mb-2 " class="form-control"> 
+            <h5 class="card-header" class = "card-body" >${task.title}</h5> 
+            <p  class = "card-body" > ${task.description}</p>
+            <a  id = "btn" type="button" class="btn btn-secondary"  data-id = ${task.id}> Estado : ${task.estado}</a>
+            <a  id = "btn2" type="button" class= "btn btn-primary" > Estado : ${task.priority}</a>
+            </div></li>`
+        }
+        return task;
 }
 
 tasks.addEventListener('click', evt =>{  
-
-    const data = evt.target.dataset;
+    //if (evt.target.tagName != 'LI') return;
     
+    console.log(evt)
+    const data = evt.target.dataset;
     const idTarea = data.id;
     const task = listaDeCard.find(task => task.id == idTarea);
 
-    if (task.estado === 'pendiente'){
+    if (task.estado === 'Pendiente'){
         task.progreso() 
-    } else if (task.estado === 'progreso') {
+    } else if (task.estado === 'Progreso') {
         task.completar();
-    }  else if (task.estado === 'realizada'){
+    }  else {
         task.volverApendiente();
     }
     
     let boton = evt.target 
         
-            boton.innerText=` Estado : ${task.estado}`
-            console.log(event.target.dataset)            
-                
+        boton.innerText=` Estado : ${task.estado}` 
+    
+    compleateCard();         
 })
 
-cambioDeColor = ()=> {
-    
-    
-    const found = listaDeCard.filter(task => task.priority ==="High" )
 
-    console.log(found)
-    
-           
-   listaDeCard.forEach(task => {
-       
-        if(task.priority ===  'High'){
-
-            document.querySelector('#tasks').className = "card text-white bg-danger mb-3"
-            
-            
-        } else {
-            document.querySelector('#tasks').className = "card text-dark bg-light mb-3" 
+const sorteableOptions = {
+    Animation: 150,
+    group: 'tareas',
+    sort: false,
+    /*onEnd: (evt) => {
+        const target = evt.to.id;
+        const idTarea = evt.item.dataset.id;
+        const task = listaDeCard.value.find((task) => task.id == idTarea);
+        
+        if (task.estado === 'Pendiente'){
+            task.progreso() 
+        } else if (task.estado === 'Progreso') {
+            task.completar();
+        }  else {
+            task.volverApendiente();
         }
     
-    })
+        localStorage.setItem('listaDeCard',JSON.stringify(listaDeCard));
+      },*/
+};
+
+    Sortable.create(taskspendiente,sorteableOptions);
+    Sortable.create(tasksprogreso, sorteableOptions);
+    Sortable.create(tasksrealizada, sorteableOptions);
+
+    compleateCard(); 
+/*CambioPrioridad = (task) => {
+
+    console.log(task)
+
+    if (task.priority === 'High'){
+        
+        task.CambioPrioridad()
+
+    } else {
+        task.ReturnPrioridad ()
+    }
 }
-
-borrar = (event) => {
-
-    event.target.parentElement.parentElement.remove();  
-}   
+*/
